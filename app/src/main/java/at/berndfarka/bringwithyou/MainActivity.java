c6deb61c1a41b5da57fcd8f10daeffd8a49e380b
Dietmar Scheidl <d.scheidl@gmx.net>
2014-11-08 09:04:03 +0100

    Dummy commit to trigger rebuild

--- app/src/main/java/at/berndfarka/bringwithyou/MainActivity.java
+++ app/src/main/java/at/berndfarka/bringwithyou/MainActivity.java
@@ -6,13 +6,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
